ee6a9d45e74241b18106fdb1def6d647bb020450
MPJBardini <jhony_bardini@hotmail.com>
2024-05-17 15:36:15 -0300

    Prime Project Java

---
+++ Meu-primeiro-projeto-java/src/PrimeProject.java
@@ -0,0 +1,18 @@
+public class PrimeProject {
+    
+public static void main(String[] args) {
+    System.out.println("Olá, JB");  
+
+    String meuNome = "JB";
+
+    int anoNascimento = 1988; 
+    int Nascimento = 1990;
+
+    boolean verdadeira = true;
+
+    anoNascimento = 1990; //mudança de variável 
+
+
+    }
+}
+
